723b0273e154542ede9572a83c471b5ad8d1fdd6
Carla Silva <carla.silva@movestock.com>
2024-02-28 21:17:53 -0300

    feature: added update and create methods

--- src/services/assessment.service.ts
+++ src/services/assessment.service.ts
@@ -16,6 +16,61 @@ export async function getAssessments(userId: string, token: string): Promise<Ass
   }
 }
 
+export async function createAssessment(
+  userId: string,
+  assessmentData: Assessment,
+  token: string): Promise<Assessment> {
+  try {
+    const response = await api.post(`/students/${userId}/assessments`, assessmentData, {
+      headers: {
+        Authorization: token
+      }
+    })
+
+    return response.data.data
+  } catch (error) {
+    console.log('Erro ao criar avaliação', error)
+    throw error
+  }
+}
+
+export async function updateAssessment(
+  userId: string,
+  assessmentId: string,
+  assessmentData: Assessment,
+  token: string): Promise<Assessment> {
+  try {
+    const response = await api.put(`/students/${userId}/assessments/${assessmentId}`, assessmentData, {
+      headers: {
+        Authorization: token
+      }
+    })
+
+    return response.data.data
+  } catch (error) {
+    console.log('Erro ao atualizar avaliação', error)
+    throw error
+  }
+}
+
+export async function getAssessmentById(
+  userId: string,
+  assessmentId: string,
+  token: string): Promise<Assessment> {
+  try {
+    const response = await api.get(`/students/${userId}/assessments/${assessmentId}`, {
+      headers: {
+        Authorization: token
+      }
+    })
+
+    return response.data.data
+  } catch (error) {
+    console.log('Erro ao obter avaliação por id:', error)
+    throw error
+  }
+}
+
 export async function deleteAssessments(userId: string, assessmentId: string, token: string): Promise<void> {
   try {
     await api.delete(`/students/${userId}/assessments/${assessmentId}`, {
